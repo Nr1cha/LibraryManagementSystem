3623276506dc9c4798860577c6720a26e5671074
Nick Richards <vrm.richards@gmail.com>
2024-02-23 15:39:03 -0700

    set up working local test

--- server.js
+++ server.js
@@ -18,16 +18,27 @@ app.use(function (err, req, res, next) {
     res.status(500).send({ message: err.message });
 });
 
-//Oauth
-app.use(
-    auth({
-        issuerBaseURL: 'https://YOUR_DOMAIN',
-        baseURL: 'https://YOUR_APPLICATION_ROOT_URL',
-        clientID: 'YOUR_CLIENT_ID',
-        secret: 'LONG_RANDOM_STRING',
-        idpLogout: true,
-    })
-);
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    if (!req.oidc.isAuthenticated()) {
+        res.redirect('/login');
+    } else {
+        res.send('Logged in');
+    }
+});
+
 
 connectToServer((err) => {
     if (err) {
